841651fefbc4e0d84ba83cd42187c18e96241b49
kemenykristof <kemeny.kristof24@gmail.com>
2020-08-29 23:32:25 +0200

    fixed bootstrap classname for navbar

--- src/App.js
+++ src/App.js
@@ -7,24 +7,21 @@ import { Route, Switch, Link } from "react-router-dom";
 function App() {
   return (
     <div>
-      <nav className="navbar bg-light navbar-expand-lg navbar"></nav>
-      <ul className="navbar-nav mr-auto">
-        <li className="navbar-item">
-          <Link className="nav-link" to="/">
-            Todos
-          </Link>
-        </li>
-        <li className="navbar-item">
-          <Link className="nav-link" to="/create">
-            Create Todo
-          </Link>
-        </li>
-      </ul>
-      <Switch>
-        <Route exact path="/" component={TodoList} />
-        <Route exact path="/edit/id" component={EditTodo} />
-        <Route exact path="/" component={CreateTodo} />
-      </Switch>
+      <nav className="navbar bg-light navbar-expand-lg navbar-light">
+        <ul className="navbar-nav mr-auto">
+          <li className="navbar-item">
+            <Link to="/" className="nav-link">Todos</Link>
+          </li>
+          <li className="navbar-item">
+            <Link to="/create" className="nav-link">Create Todo</Link>
+          </li>
+        </ul>
+      </nav>
+    <Switch>
+      <Route exact path="/" component={TodoList}/>
+      <Route path="/edit/:id" component={EditTodo}/>
+      <Route path="/create" component={CreateTodo}/>
+    </Switch>
     </div>
   );
 }
